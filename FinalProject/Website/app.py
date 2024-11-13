625f1b0cf17370f50a8341f4bc04db52b29a707e
Darby Lane <166237578+dlane13@users.noreply.github.com>
2024-11-12 20:52:32 -0500

    Committed changes to app.py, conveyorbelt.html, and homepage.html

--- FinalProject/Website/app.py
+++ FinalProject/Website/app.py
@@ -9,16 +9,27 @@ app = Flask(__name__)
 def home():
     return render_template("HomePage.html")
 
-@app.route("/conveyor_button", methods=["GET"])
-def conveyor_button():
-    print("The conveyor belt is running...")
-    time.sleep(5)
-    return "The conveyor belt has finished running. Your printer is ready for your next crazy idea!"
-
 @app.route('/HomePage', methods=['GET'])
 def HomePage():
-    return render_template("HomePage.html", name="Darby Lane")
+    return render_template("HomePage.html")
 
 @app.route('/ConveyorBelt', methods=['GET'])
 def ConveyorBelt():
-    return render_template("ConveyorBelt.html", name="Darby Lane")
+    return render_template("ConveyorBelt.html")
+
+@app.route('/click-button', methods=["GET"])
+def click_button():
+    return json.dumps(print_conveyor_status())
+
+def print_conveyor_status():
+    print("Running the conveyor belt now!")
+    time.sleep(5)
+    return "Conveyor belt has finished running! The printer is ready for your next project."
+
+@app.route('/OctoPrint', methods=["GET"])
+def OctoPrint():
+    return render_template("OctoPrint.html")
+
+@app.route('/FailureDetection', methods=["GET"])
+def FailureDetection():
+    return render_template("FailureDetection.html")
